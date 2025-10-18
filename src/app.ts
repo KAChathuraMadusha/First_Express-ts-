00ba778c8338838a37270f8c8c662c7ac94d1a10
K.A Chathura Madusha <chathuramadusha20@gmail.com>
2025-10-18 13:43:44 +0530

    initial commit

---
+++ src/app.ts
@@ -0,0 +1,16 @@
+import express, { Application, Request, Response } from "express";
+
+const app: Application = express();
+
+app.use(express.json());
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello World");
+});
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
+
